3f0b57711f97daef617be9d67acbc1f9c844ca9d
hero-7-dev <82283255+hero-7-dev@users.noreply.github.com>
2021-12-04 01:04:19 +0200

    App Code

---
+++ Employee/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Employee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblChooseFile = new System.Windows.Forms.Label();
+            this.TxtFileName = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BtnChooseFile = new System.Windows.Forms.Button();
+            this.EmployeeGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblChooseFile
+            // 
+            this.LblChooseFile.AutoSize = true;
+            this.LblChooseFile.Location = new System.Drawing.Point(12, 25);
+            this.LblChooseFile.Name = "LblChooseFile";
+            this.LblChooseFile.Size = new System.Drawing.Size(97, 13);
+            this.LblChooseFile.TabIndex = 1;
+            this.LblChooseFile.Text = "Please Choose File";
+            // 
+            // TxtFileName
+            // 
+            this.TxtFileName.AllowDrop = true;
+            this.TxtFileName.Location = new System.Drawing.Point(112, 22);
+            this.TxtFileName.Name = "TxtFileName";
+            this.TxtFileName.ReadOnly = true;
+            this.TxtFileName.Size = new System.Drawing.Size(211, 20);
+            this.TxtFileName.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // BtnChooseFile
+            // 
+            this.BtnChooseFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnChooseFile.Location = new System.Drawing.Point(329, 21);
+            this.BtnChooseFile.Name = "BtnChooseFile";
+            this.BtnChooseFile.Size = new System.Drawing.Size(33, 23);
+            this.BtnChooseFile.TabIndex = 2;
+            this.BtnChooseFile.Text = "...";
+            this.BtnChooseFile.UseVisualStyleBackColor = true;
+            this.BtnChooseFile.Click += new System.EventHandler(this.BtnChooseFile_Click);
+            // 
+            // EmployeeGrid
+            // 
+            this.EmployeeGrid.AllowUserToAddRows = false;
+            this.EmployeeGrid.AllowUserToDeleteRows = false;
+            this.EmployeeGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.EmployeeGrid.Location = new System.Drawing.Point(3, 71);
+            this.EmployeeGrid.Name = "EmployeeGrid";
+            this.EmployeeGrid.ReadOnly = true;
+            this.EmployeeGrid.Size = new System.Drawing.Size(426, 123);
+            this.EmployeeGrid.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(433, 201);
+            this.Controls.Add(this.EmployeeGrid);
+            this.Controls.Add(this.BtnChooseFile);
+            this.Controls.Add(this.LblChooseFile);
+            this.Controls.Add(this.TxtFileName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.EmployeeGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblChooseFile;
+        private System.Windows.Forms.TextBox TxtFileName;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button BtnChooseFile;
+        private System.Windows.Forms.DataGridView EmployeeGrid;
+    }
+}
+
---
+++ Employee/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Employee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public DataTable ConvertToDataTable(string filePath)
+        {
+            dt.Clear();
+            dt.Columns.Clear();
+            dt.Columns.Add("EmpID");
+            dt.Columns.Add("ProjectID");
+            dt.Columns.Add("DateFrom");
+            dt.Columns.Add("DateTo");
+            dt.Columns.Add("TotalDays");
+    
+
+
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                var cols = line.Split(',');
+
+                DataRow _ravi = dt.NewRow();
+                for (int cIndex = 0; cIndex < 4; cIndex++)
+                {
+                    if(cols[3].ToString()=="NULL" )
+                    {  DateTime date = DateTime.Now;
+                        cols[3]=date.ToString("yyyy-MM-dd");
+                    }
+
+                    _ravi[cIndex] = cols[cIndex];
+
+                    if (cIndex==3)
+                    {
+                        DateTime d1 = Convert.ToDateTime(cols[3].ToString());
+                        DateTime d2 = Convert.ToDateTime(cols[2].ToString());
+                        
+                        _ravi[4] =(d1 - d2).Days; 
+                    }
+                }
+
+                dt.Rows.Add(_ravi);
+            }
+
+            return dt;
+        }
+        //create list of data
+        public class Project
+        {
+            public int EmpID { get; set; }
+            public int ProjectID { get; set; }
+            public int TotalDays { get; set; }
+        }
+        public class Pairofemployees
+        {
+            public int EmpID1 { get; set; }
+            public int EmpID2 { get; set; }
+            public int ProjectID { get; set; }
+            public int Daysworked { get; set; }
+        }
+
+        // Create Data Table
+        DataTable dt = new DataTable();
+        /// <summary>
+        ///  Grid layout
+        /// </summary>
+        public void Gridyout()
+        {
+            EmployeeGrid.Columns["EmpID1"].HeaderText = " Employee ID #1";
+            EmployeeGrid.Columns["EmpID2"].HeaderText = " Employee ID #2";
+            EmployeeGrid.Columns["ProjectID"].HeaderText = "Project ID";
+            EmployeeGrid.Columns["Daysworked"].HeaderText = " Days worked";
+        }
+        private void BtnChooseFile_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog theDialog = new OpenFileDialog();
+            theDialog.Title = "Open Text File";
+            theDialog.Filter = "TXT files|*.txt";
+            theDialog.InitialDirectory = @"C:\";
+            if (theDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    TxtFileName.Text = theDialog.FileName;
+                    if ((myStream = theDialog.OpenFile()) != null)
+                    {
+
+                        DataTable Rdt= ConvertToDataTable(theDialog.FileName);
+
+
+                            List<Project> ProjecttList = new List<Project>();
+                            List<Pairofemployees> PairofemployeesList = new List<Pairofemployees>();
+                            for (int i = 0; i < Rdt.Rows.Count; i++)
+                            {
+                                Project project = new Project();
+                                project.EmpID  = Convert.ToInt32(Rdt.Rows[i]["EmpID"]);
+                                project.ProjectID  =int.Parse(Rdt.Rows[i]["ProjectID"].ToString());
+                                project.TotalDays = int.Parse(Rdt.Rows[i]["TotalDays"].ToString());
+
+                                ProjecttList.Add(project);
+                            }
+                            foreach (IGrouping<int, Project> project in ProjecttList.GroupBy(x => x.ProjectID).ToList())
+                            {
+                                List<Project> projectGroup = project.Take(2).ToList();
+                                if (projectGroup.Count == 2)
+                                {
+                                    Pairofemployees pairofemployees = new Pairofemployees();
+                                    {
+
+                                      
+                                        pairofemployees.EmpID1 = projectGroup[0].EmpID;
+                                        pairofemployees.EmpID2 = projectGroup[1].EmpID;
+                                        pairofemployees.ProjectID = projectGroup[0].ProjectID;
+                                        pairofemployees.Daysworked = projectGroup.Min(x => x.TotalDays);
+
+                                    };
+                                    PairofemployeesList.Add(pairofemployees);
+                                }
+                            }
+
+                            
+                            List<Pairofemployees> Toplist = new List<Pairofemployees>();
+                            Toplist = PairofemployeesList.OrderByDescending(x => x.Daysworked).ToList();
+
+                            var source = new BindingSource();
+                            source.DataSource = Toplist.First();
+                            EmployeeGrid.DataSource = source;
+                            EmployeeGrid.Refresh();
+                            Gridyout();
+                       
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
